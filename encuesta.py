ebec4c5ace4a74a96c7a6c9f1721c378beb6c7fd
Juan Esteban Argüello Botero <juanarguello0526@gmail.com>
2026-04-13 20:10:56 -0500

    first commit

--- encuesta.py
+++ encuesta.py
@@ -12,11 +12,13 @@ while continuar:
     edad = int(input("Edad: "))
     genero = input("Genero (M/F/O): ")
     
-    while True:
+    calif_valida = False
+    while not calif_valida:
         calif = int(input("Calificacion (1-5): "))
         if calif >= 1 and calif <= 5:
-            break
-        print("Error: La calificacion debe estar entre 1 y 5")
+            calif_valida = True
+        else:
+            print("Error: La calificacion debe estar entre 1 y 5")
     
     persona = {
         "nombre": nombre,
